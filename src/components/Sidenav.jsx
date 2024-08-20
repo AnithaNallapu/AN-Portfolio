cee237a43bc11f6548d51b7199d6cd6272852fc0
AnithaNallapu <anithanallapu75@gmail.com>
2024-08-20 21:24:03 +0100

    My Portfolio

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useState } from 'react'
 import { AiOutlineMail, AiOutlineMenu, AiOutlineProject } from 'react-icons/ai'
 import { AiOutlineHome } from 'react-icons/ai'
-import { BsPerson } from 'react-icons/bs'
+import { GrCertificate } from 'react-icons/bs'
 import { GrProjects } from 'react-icons/gr'
 
 export default function Sidenav() {
@@ -30,7 +30,7 @@ export default function Sidenav() {
               <span className='pl-4'>Projects</span>
             </a>
             <a onClick={handleNav} href="#main" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
-              <BsPerson size={20} />
+              <GrCertificate size={20} />
               <span className='pl-4'>Resume</span>
             </a>
             <a onClick={handleNav} href="#contact" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
@@ -56,7 +56,7 @@ export default function Sidenav() {
             <AiOutlineProject size={20}/>
           </a>
           <a href="#main" className='rounded-full shadow-lg bg-gray-100 shadow-gray-100 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
-            <BsPerson size={20}/>
+            <GrCertificate size={20}/>
           </a>
           <a href="#contact" className='rounded-full shadow-lg bg-gray-100 shadow-gray-100 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
             <AiOutlineMail size={20}/>
