a18a8504bf1d6872fdd41fdf25566687bad1e6d0
AnithaNallapu <anithanallapu75@gmail.com>
2024-08-20 21:34:18 +0100

    Added Certificate folder

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -31,7 +31,7 @@ export default function Sidenav() {
             </a>
             <a onClick={handleNav} href="#main" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
               <GrCertificate size={20} />
-              <span className='pl-4'>Resume</span>
+              <span className='pl-4'>Certificates</span>
             </a>
             <a onClick={handleNav} href="#contact" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
               <AiOutlineMail size={20} />
